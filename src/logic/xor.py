35496e982679d14625baadbed21eca02d3959719
Masashi Kimura <kimura@convergence-lab.com>
2018-04-27 14:45:42 +0900

    initial commit

---
+++ src/logic/xor.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import argparse
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--epochs', type=int, default=10000, metavar='N',
+                    help='number of epochs to train (default: 1000)')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+args = parser.parse_args()
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.manual_seed(args.seed)
+
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc = nn.Linear(2, 1)
+
+    def forward(self, x):
+        x = F.sigmoid(self.fc(x))
+        return x
+
+model = Net().to(device)
+inputs = list(map(lambda s: torch.Tensor([s]), [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1]
+]))
+targets = list(map(lambda s: torch.Tensor([s]), [
+    [0],
+    [1],
+    [1],
+    [0]
+]))
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr)
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(zip(inputs, targets)):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.mse_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(inputs),
+                100. * batch_idx / len(inputs), loss.item()))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in zip(inputs, targets):
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.mse_loss(output, target, size_average=False).item() # sum up batch loss
+            pred = output[0].type(torch.int8)
+            correct += pred.eq(target.type(torch.int8)).item()
+
+    test_loss /= len(inputs)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(inputs),
+        100. * correct / len(targets)))
+
+
+for epoch in range(1, args.epochs + 1):
+    train(epoch)
+    test()
